2ecbdc1d6dd8a0f62750933ef5d080b0c9b97809
Victor <vlopezmoles@gmail.com>
2022-09-19 22:25:57 +0200

    server up

--- app/app.js
+++ app/app.js
@@ -1,1 +1,20 @@
-console.log('Hello World');
+const express = require('express');
+const app = express();
+const route = requite('./routes/routes');
+
+//Settings
+app.set('port', 3000);
+app.set('json spaces', 2);
+
+//Middleware
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+//Routes
+app.use('/', route);
+
+//Start the server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
