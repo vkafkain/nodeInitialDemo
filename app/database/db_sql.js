5890b538eaf226d42a4172473dc4d46d91274ff4
Victor <vlopezmoles@gmail.com>
2022-09-30 21:29:39 +0200

    update dbconnection

--- app/database/db_sql.js
+++ app/database/db_sql.js
@@ -1,10 +1,13 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('dados', 'root', '1234', {
+const sequelize = new Sequelize('dados', 'root', '', {
     host: 'localhost',
     dialect: 'mysql'
 });
-
+sequelize.sync({ force: false })
+    .then(() => {
+        console.log("Tablas sincronizadas")
+    });
 //TODO create table if not exist
 
 module.exports = sequelize;
