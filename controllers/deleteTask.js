808001ad12781732c8b55b9e681b42b66ff0f413
AndreTruss <andrea.trussardi@gmail.com>
2022-09-30 19:57:13 +0200

    deleteOneTask

--- controllers/deleteTask.js
+++ controllers/deleteTask.js
@@ -15,12 +15,6 @@ const deleteTask = async ( userLogin ) => {
         });
 
         const questions = [
-            {
-                type: 'list',
-                name: 'deleteTasks',
-                message: '¿Quieres borrar todas tus tareas?',
-                choices: [ 'Sì', 'No' ],
-              },
               {
                 type: 'rawlist',
                 name: 'deleteOneTask',
@@ -29,20 +23,10 @@ const deleteTask = async ( userLogin ) => {
               },  
         ]
         
-        await inquirer.prompt(questions).then((answers) => {
-          return answers;
-        });
+        const answer = await inquirer.prompt(questions);
 
-
-        if ( (findUser !== undefined) && (answers.deleteTasks === 'Sì') ) {
-            findUser.tasks = [];
-            data = JSON.stringify(data);
-            await writeFile('./databases/database.json', data);
-
-            console.log('Tasks deleted!');
-        }
-        if ( (findUser !== undefined) && (answers.deleteTasks === 'No') ) {
-            const index = findUser.tasks.findIndex( (task) => task === answers.deleteOneTask );
+        if ( findUser !== undefined ) {
+            const index = titleTasks.findIndex( (task) => task === answer.deleteOneTask );
             findUser.tasks.splice( index, 1 );
             data = JSON.stringify(data);
             await writeFile('./databases/database.json', data);
