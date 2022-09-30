3fa3a3cf98d138a7e60f02da0a507491c249e4e5
Victor <vlopezmoles@gmail.com>
2022-09-30 17:26:10 +0200

    update routes

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -4,13 +4,22 @@ const router =  Router();
 
 //GET /players: retorna el llistat de tots els jugadors/es del sistema amb el seu percentatge d’èxits.
 router.get('/players', getPlayers);
+
 //POST /players: crea un jugador/a.
 router.post('/players', createPlayer);
+
 //PUT /players/{id}: modifica el nom del jugador/a.
 router.put('/players/:id', updatePlayer);
+
 //POST /games/{id}: un jugador/a específic realitza una tirada.
 router.post('/games/:id', playerRoll);
+
 //DELETE /games/{id}: elimina les tirades del jugador/a.
 router.delete('/games/:id', deleteGames)
+
+// GET /games/{id}: retorna el llistat de jugades per un jugador/a.
+
+
+
 
 module.exports = router;
