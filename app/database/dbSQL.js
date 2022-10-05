fe034141ff8696864a8a064b75aa8c2af66a8fc5
Victor <vlopezmoles@gmail.com>
2022-10-05 16:09:50 +0200

    init connection to DB

--- app/database/dbSQL.js
+++ app/database/dbSQL.js
@@ -1,8 +1,21 @@
 const { Sequelize } = require('sequelize');
+const mysql = require('mysql2');
 
+
+async function connecSQLDB() {
+    
+    const connection = mysql.createConnection({ host:'localhost', user: 'root', password: '1234',});
+    connection.query(`CREATE DATABASE IF NOT EXISTS dados;`, (err, result) =>{
+        if(err) throw err
+        db.sync({ force:true })
+        .then(()=> console.log('Database Connected'))
+        .catch(err => console.log(err))
+    })
+    connection.end();
+}
 const db = new Sequelize('dados', 'root', '1234', {
     host: 'localhost',
     dialect: 'mysql'
 });
 
-module.exports = db;
+module.exports = {db, connecSQLDB};
