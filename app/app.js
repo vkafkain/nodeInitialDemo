492d0e90ababd0b90cbc52510516663491ddae2d
vkafkain <vlopezmoles@gmail.com>
2022-10-06 10:33:38 +0200

    added log database connected

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ const app = express();
 
 //middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 //routes
 app.use(playerRoutes);
 
--- app/index.js
+++ app/index.js
@@ -1,10 +1,9 @@
 const app = require('./app')
-const {db, connecSQLDB} = require('./database/dbSQL');
+const { connecSQLDB } = require('./database/dbSQL');
 
 async function main() {
   try{  
     await connecSQLDB();
-    await db.sync({force: true})
     app.listen(3000);
     console.log("Server is listening on port", 3000);
 } catch(error) {
