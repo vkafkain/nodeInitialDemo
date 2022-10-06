6cdfffb976c5913b088ca7863b65ecd66a5618c1
Victor <vlopezmoles@gmail.com>
2022-10-07 00:02:56 +0200

    init services rework files

--- app/services/letsRoll.js
+++ app/services/letsRoll.js
@@ -1,8 +1,8 @@
 const rollDices = () => {
-    const dice1 = Math.floor(6* Math.random()) + 1;
-    const dice2 = Math.floor(6 * Math.random()) + 1;
-    const veredict = dice1 + dice2 === 7 ? 'win' : 'lose';
-    return { dice1, dice2, rollScore: dice1 + dice2, veredict };
+  const dice1 = Math.floor(6 * Math.random()) + 1;
+  const dice2 = Math.floor(6 * Math.random()) + 1;
+  const veredict = dice1 + dice2 === 7 ? "win" : "lose";
+  return { dice1, dice2, rollScore: dice1 + dice2, veredict };
 };
 
 module.exports = rollDices;
