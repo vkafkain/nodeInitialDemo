e90bb4ff9a4bdaa22549462123597806480bf589
Victor <vlopezmoles@gmail.com>
2022-10-07 00:02:06 +0200

    rework router files

--- app/routes/games.js
+++ app/routes/games.js
@@ -1,14 +1,18 @@
-const {Router} = require('express');
-const {  playerRoll, deleteGames, getGames } = require('../controllers/controllersMysql')
-const router =  Router();
+const { Router } = require("express");
+const {
+  playerRoll,
+  deleteGames,
+  getGames,
+} = require("../controllers/gameControllers");
+const router = Router();
 
 //POST /games/{id}: un jugador/a específic realitza una tirada.
-router.post('/:id', playerRoll);
+router.post("/:id", playerRoll);
 
 //DELETE /games/{id}: elimina les tirades del jugador/a.
-router.delete('/:id', deleteGames);
+router.delete("/:id", deleteGames);
 
-// GET /games/{id}: retorna el llistat de jugades per un jugador/a. 
-router.get('/:id', getGames);
+// GET /games/{id}: retorna el llistat de jugades per un jugador/a.
+router.get("/:id", getGames);
 
 module.exports = router;
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,14 +1,11 @@
-const {Router} = require('express');
-const router =  Router();
-const playerRouter = require('./players');
-const gamesRouter = require('./games');
-const rankingRouter = require('./ranking');
+const { Router } = require("express");
+const router = Router();
+const playerRouter = require("./players");
+const gamesRouter = require("./games");
+const rankingRouter = require("./ranking");
 
-router.use('/players', playerRouter);
-router.use('/games', gamesRouter);
-router.use('/ranking', rankingRouter);
-
-//TODO dar nombre anonimo a los anonimos
-//TODO revisar tabulacion etc
+router.use("/players", playerRouter);
+router.use("/games", gamesRouter);
+router.use("/ranking", rankingRouter);
 
 module.exports = router;
