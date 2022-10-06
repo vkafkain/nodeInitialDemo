48642bbe1f2b6c6999dfe7eb33903a0c4841cfb3
vkafkain <vlopezmoles@gmail.com>
2022-10-06 12:31:10 +0200

    app routes rework

--- app/app.js
+++ app/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const routes = require('./routes/routes')
 const app = express();
 
-
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
