c638fa5b54ebda2049515d129ecdad24d7718281
vkafkain <vlopezmoles@gmail.com>
2022-10-06 10:31:59 +0200

    fixed error when creating the database

--- app/database/dbSQL.js
+++ app/database/dbSQL.js
@@ -1,21 +1,21 @@
 const { Sequelize } = require('sequelize');
 const mysql = require('mysql2');
 
-async function connecSQLDB() {
-    
-    const connection = mysql.createConnection({ host:'localhost', user: 'root', password: '1234',});
-    connection.query(`CREATE DATABASE IF NOT EXISTS dados;`, (err, result) =>{
-        if(err) throw err
-        db.sync({ force:false })
-        .then(()=> console.log('Database Connected'))
-        .catch(err => console.log(err))
-    })
-    connection.end();
-}
 const db = new Sequelize('dados', 'root', '1234', {
     host: 'localhost',
     dialect: 'mysql'
 });
-
-
+async function connecSQLDB() {
+    
+    const connection = mysql.createConnection({ host:'localhost', user: 'root', password: '1234',});
+    connection.query(`CREATE DATABASE IF NOT EXISTS dados;`, (err, result) =>{
+        console.log('Database created');
+        if(err) throw err
+        db.sync({ force:false })
+        .then(()=> console.log('Database connected'))
+        .catch(err => console.log(err))
+    })
+    connection.end();
+}
+  
 module.exports = {db, connecSQLDB};
