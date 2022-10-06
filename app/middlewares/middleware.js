8ffdc0d0d47483f04f8044672fe5d5538db9b7fd
Victor <vlopezmoles@gmail.com>
2022-10-07 00:50:58 +0200

    init middleware JWT

--- app/middlewares/middleware.js
+++ app/middlewares/middleware.js
@@ -1,3 +1,9 @@
+const jwt = require('jsonwebtoken');
+
+
+jwt.sign({ player: 'bar' }, privateKey, { algorithm: 'RS256' }, function(err, token) {
+    console.log(token);
+  });
+
 //TODO JWT autentificador
 //TODO dar nombre anonimo a los anonimos
-//TODO revisar tabulacion etc
