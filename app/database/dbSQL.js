5b4168cb355e61081920bc42c6ef5497ac4768f7
Victor <vlopezmoles@gmail.com>
2022-10-06 07:23:16 +0200

    update routes

--- app/database/dbSQL.js
+++ app/database/dbSQL.js
@@ -1,13 +1,12 @@
 const { Sequelize } = require('sequelize');
 const mysql = require('mysql2');
 
-
 async function connecSQLDB() {
     
     const connection = mysql.createConnection({ host:'localhost', user: 'root', password: '1234',});
     connection.query(`CREATE DATABASE IF NOT EXISTS dados;`, (err, result) =>{
         if(err) throw err
-        db.sync({ force:true })
+        db.sync({ force:false })
         .then(()=> console.log('Database Connected'))
         .catch(err => console.log(err))
     })
@@ -17,5 +16,6 @@ const db = new Sequelize('dados', 'root', '1234', {
     host: 'localhost',
     dialect: 'mysql'
 });
+
 
 module.exports = {db, connecSQLDB};
