797ed6872048dd0f67e27c45bcf4425470afea6e
Victor <vlopezmoles@gmail.com>
2022-10-07 00:01:17 +0200

    update db

--- app/database/dbSQL.js
+++ app/database/dbSQL.js
@@ -1,22 +1,28 @@
-const { Sequelize } = require('sequelize');
-const mysql = require('mysql2');
+const { Sequelize } = require("sequelize");
+const mysql = require("mysql2");
+const config = require("../config/database");
 
-const db = new Sequelize('dados', 'root', '1234', {
-    host: 'localhost',
-    dialect: 'mysql'
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+  host: config.host,
+  dialect: config.dialect,
 });
 async function connecSQLDB() {
-    
-    const connection = mysql.createConnection({ host:'localhost', user: 'root', password: '1234',});
-    connection.query(`CREATE DATABASE IF NOT EXISTS dados;`, (err, result) =>{
-        console.log('Database created');
-        if(err) throw err
-        db.sync({ force:false })
-        .then(()=> console.log('Database connected'))
-        .catch(err => console.log(err))
-    })
-    connection.end();
+  const connection = mysql.createConnection({
+    host: config.host,
+    user: config.username,
+    password: config.password,
+  });
+  connection.query(
+    `CREATE DATABASE IF NOT EXISTS ${config.database};`,
+    (err, result) => {
+      console.log("Database created");
+      if (err) throw err;
+      sequelize.sync({ force: false })
+        .then(() => console.log("Database connected"))
+        .catch((err) => console.log(err));
+    }
+  );
+  connection.end();
 }
 
-  
-module.exports = {db, connecSQLDB};
+module.exports = { sequelize, connecSQLDB };
