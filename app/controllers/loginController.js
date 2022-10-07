4397209aff519728820c66954a45cda891d07daa
Victor <vlopezmoles@gmail.com>
2022-10-07 12:49:34 +0200

    login Controllers create Admin added, and jwt.signt

--- app/controllers/loginController.js
+++ app/controllers/loginController.js
@@ -1,4 +1,34 @@
 const { Player } = require("../models/Player");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const loginConfig = require("../config/login");
 
+const createAdmin = (req, res) => {
+  let name = req.body.name;
+  if (name !== "admin") {
+    res.status(401).json({ msg: "Unauthorized name" });
+  }
+  let password = bcrypt.hashSync(
+    req.body.password,
+    Number.parseInt(loginConfig.rounds)
+  );
 
+  Player.create({
+    name: name,
+    password: password,
+  })
+    .then((user) => {
+      let token = jwt.sign({ user: user }, loginConfig.secret, {
+        expiresIn: loginConfig.expires,
+      });
+      res.json({
+        user: user,
+        token: token,
+      });
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
+
+module.exports = { createAdmin };
