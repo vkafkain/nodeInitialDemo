38cae14b142fe73be34f759f0970984ba58fba69
Victor <vlopezmoles@gmail.com>
2022-10-07 12:50:32 +0200

    init middlewares login

---
+++ app/middlewares/login.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/login");
+
+const auth = (req, res, next) => {
+  if (!req.headers.authorization) {
+    res.status(401).json({ msg: "Unauthorized access" });
+  } else {
+    let token = req.headers.authorization.split(" ")[1];
+
+    jwt.verify(token, config.secret, (err, decoded) => {
+      if (err) {
+        res.status(400).json({ msg: "Error Encoding", err });
+      } else {
+        console.log(decoded);
+        next();
+      }
+    });
+  }
+};
+
+module.exports = auth;
+
+//TODO JWT autentificador
+//TODO dar nombre anonimo a los anonimos
--- app/middlewares/middleware.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-
-jwt.sign({ player: 'bar' }, privateKey, { algorithm: 'RS256' }, function(err, token) {
-    console.log(token);
-  });
-
-//TODO JWT autentificador
-//TODO dar nombre anonimo a los anonimos
