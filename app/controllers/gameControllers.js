2a499fbc947f56d9702be4d28e9687d623061950
Victor <vlopezmoles@gmail.com>
2022-10-09 01:13:01 +0200

    rework roll dices controller old version bugged

--- app/controllers/gameControllers.js
+++ app/controllers/gameControllers.js
@@ -5,7 +5,7 @@ const rollDices = require("../services/letsRoll");
 const playerRoll = async (req, res) => {
   const PlayerId = req.params.id;
   const { dice1, dice2, rollScore, veredict } = rollDices();
-/*   try {
+  try {
     const roll = await Game.create({
       dice1,
       dice2,
@@ -14,7 +14,10 @@ const playerRoll = async (req, res) => {
       PlayerId,
     });
 
-    let arrayGames = [];
+    const playerRolled = await Player.findAll({
+      attributes: ["name"],
+      where: { id: PlayerId },
+    });
 
     if (veredict === "win") {
       Player.increment(["gamesPlayed", "gamesWin"], {
@@ -24,33 +27,23 @@ const playerRoll = async (req, res) => {
     if (veredict === "lose") {
       Player.increment(["gamesPlayed"], { where: { id: PlayerId } });
     }
-    const player = await Player.findAll({
-      attributes: ["gamesPlayed", "gamesWin"],
-      where: { id: PlayerId },
+
+    const countGames = await Game.count({ where: { PlayerId: PlayerId } });
+    const countWins = await Game.count({
+      where: { PlayerId: PlayerId, veredict: "win" },
     });
-    console.log(player);
-
-    arrayGames.push(player);
-
-    const { gamesPlayed, gamesWin } = arrayGames[0][0].dataValues;
-    console.log(gamesWin, gamesPlayed);
-    const winRate = (gamesWin / gamesPlayed) * 100;
-
-    await Player.update({ winRate }, { where: { id: PlayerId } });
-    console.log(player);
-
-    const playerRolled = await Player.findAll({
-      attributes: ["name"],
-      where: { id: PlayerId },
-    });
-
+    console.log(countGames, countWins);
+    await Player.update(
+      { winRate: (countWins / countGames) * 100 },
+      { where: { id: PlayerId } }
+    );
     res.status(200).json({
       playerRolled,
       roll,
     });
   } catch (error) {
-    return res.status(400).json({ message: "Game Error" });
-  } */
+    return res.status(400).json({ error });
+  }
 };
 
 const deleteGames = async (req, res) => {
@@ -61,7 +54,7 @@ const deleteGames = async (req, res) => {
 
     await Player.update(
       {
-        games: 0,
+        gamesPlayed: 0,
         gamesWin: 0,
         winRate: 0,
       },
@@ -82,10 +75,15 @@ const deleteGames = async (req, res) => {
 const getGames = async (req, res) => {
   const idPlayer = req.params.id;
   try {
-    const games = await Game.findAll({
-      where: { playerId: idPlayer },
+    const player = await Player.findAll({
+      attributes: ["name"],
+      where: { id: idPlayer },
     });
-    res.send({ games });
+
+    const games = await Game.findAll({
+      where: { PlayerId: idPlayer },
+    });
+    res.status(200).json({ player, games });
   } catch (error) {
     return res.status(404).json({ message: error });
   }
