9464a00f97ec971bbedaa25c9bff95c6894681e0
Victor <vlopezmoles@gmail.com>
2022-10-09 01:27:24 +0200

    format page

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const { connecSQLDB } = require("./database/dbSQL");
-const config = require('./config/database');
+const config = require("./config/database");
 
 async function main() {
   try {
