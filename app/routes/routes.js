09b4266c000a58e6db01faa6401b085b9aee2e41
Victor <vlopezmoles@gmail.com>
2022-10-09 03:04:49 +0200

    update routes

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,10 +1,10 @@
 const { Router } = require("express");
+const auth = require("../middlewares/login");
 const router = Router();
 const playerRouter = require("./players");
 const gamesRouter = require("./games");
 const rankingRouter = require("./ranking");
 const loginRouter = require("./login");
-const auth = require("../middlewares/login");
 
 router.use("/players", auth, playerRouter);
 router.use("/games", auth, gamesRouter);
