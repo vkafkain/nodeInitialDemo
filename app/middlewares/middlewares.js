fc1fa3df7e325596cfdc5a44cab697dcd44324c6
Victor Kafka <vkafka@Mac-mini.home>
2022-11-23 13:01:04 +0100

    añadimos cambios tras la corrección de Omar

--- app/middlewares/middlewares.js
+++ app/middlewares/middlewares.js
@@ -1,27 +1,3 @@
-const multer = require('multer');
-const path = require('path');
-
-//upload
-const storage = multer.diskStorage({
-    destination: path.join(__dirname, '../public/uploads'),
-    filename: (req, file, cb) => {
-        cb(null, file.originalname);
-    }
-});
-const upload = multer({
-    storage,
-    dest: path.join(__dirname, 'public/uploads'),
-    limits:{fileSize: 50000000},
-    fileFilter: (req, file, cb) => {
-        const fileTypes = /jpeg|jpg|png|gif/;
-        const mimetype = fileTypes.test(file.mimetype);
-        const extname = fileTypes.test(path.extname(file.originalname)); 
-        if(mimetype && extname){
-            return cb(null, true);
-        }
-        cb("Error: Wrong file type, file has to be jpeg, jpg, png or gif");
-    }
-}).single('images');
 
 //time(cache-control)
 const cacheControl = (req, res, next)=>{
@@ -44,4 +20,4 @@ const authentification = (req, res, next)=> {
     next();
 }
 
-module.exports = {upload, cacheControl, authentification};
+module.exports = { cacheControl, authentification};
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,14 +1,14 @@
 const {Router} = require('express');
 const cors = require('cors');
-const {uploadPost, user, time, pokemon} = require('../controllers/controllers');
-const {upload, cacheControl, authentification} = require('../middlewares/middlewares');
+const {uploadPost, user, time, pokemon, error404} = require('../controllers/controllers');
+const {cacheControl, authentification} = require('../middlewares/middlewares');
 const router =  Router();
 
 //http://localhost:3000/user
 router.get('/user', user);
 
 //http://localhost:3000/upload
-router.post('/upload', upload, uploadPost, (req, res) => {
+router.post('/upload', uploadPost, (req, res) => {
     console.log(req.file);
     res.send('uploaded');
 });
@@ -18,6 +18,12 @@ router.post('/time', cors(), cacheControl, authentification, time);
 
 //http://localhost:3000//pokemon/{id}
 router.get('/pokemon/:id', pokemon);
+
+//rutas no especificadas
+router.get("/*", error404);
+router.post("/*", error404)
+router.put("/*", error404)
+router.delete("/*", error404)
 
 
 module.exports = router;
