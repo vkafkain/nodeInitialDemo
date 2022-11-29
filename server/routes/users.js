817d9dc97760d509f1fb73af32fbfbfd4d798216
vkafkain <vlopezmoles@gmail.com>
2022-11-29 10:18:18 +0100

    limpiando comentarios

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,10 +5,8 @@ const { authentication } = require("../middlewares/authentication");
 
 //registro usuario
 router.post("/register", userControllers.register);
+
 //login usuario
 router.post("/login", authentication, userControllers.login);
-//change password
-// router.post("/changepassword", userControllers.changePassword);
-//profile user
-// router.post("/profileuser", userControllers.profileUser);
+
 module.exports = router;
