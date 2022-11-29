665cc32b84ac34a5e5de6fc7c72a163f5324b0bb
Victor Kafka <vkafka@Mac-mini.home>
2022-11-29 12:50:51 +0100

    iniciamos google Auth

--- client/app.js
+++ client/app.js
@@ -7,7 +7,7 @@ const PORT = 8080;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'./public/index.html'))
+    res.sendFile(path.join(__dirname,'./public/html/index.html'))
   })
 
 app.listen(PORT,() => {
--- server/app.js
+++ server/app.js
@@ -4,8 +4,11 @@ const express = require("express");
 const app = express();
 const http = require("http").Server(app);
 const connectDB = require("./database/connectDB");
-const sockets = require("./sockets/sockets")
+const sockets = require("./sockets/sockets");
 const routes = require("./routes/routes");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+const { addAbortSignal } = require("stream");
 
 const io = require("socket.io")(http, {
   cors: {
@@ -19,17 +22,31 @@ connectDB();
 // Middlewares
 app.use(express.json());
 app.use(cors());
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["cyberwolf"],
+    maxAge: 24 * 60 * 60 * 100,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // Routes
-app.use(routes)
+app.use(routes);
 
 //invalid route handling
 app.use((req, res) =>
-res.status(404).send({ status: "fail", message: "PAGE NOT FOUND" })
+  res.status(404).send({ status: "fail", message: "PAGE NOT FOUND" })
 );
 
 // Sockets
 sockets(io);
 
 PORT = process.env.SERVER_PORT || 3000;
-http.listen(PORT, console.log(`Server listening on port ${process.env.SERVER_PORT}`));
+http.listen(
+  PORT,
+  console.log(`Server listening on port ${process.env.SERVER_PORT}`)
+);
