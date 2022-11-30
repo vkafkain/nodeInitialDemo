b8e0c508fdd4f241de2c6f55bc5f7a4ca3efa339
Elías <eliashz@gmail.com>
2022-11-30 09:27:03 +0100

    muevo codigo de google auth a nueva rama para que el chat funcione

--- server/app.js
+++ server/app.js
@@ -6,10 +6,7 @@ const http = require("http").Server(app);
 const connectDB = require("./database/connectDB");
 const sockets = require("./sockets/sockets");
 const routes = require("./routes/routes");
-const passport = require("passport");
-const cookieSession = require("cookie-session");
-const { addAbortSignal } = require("stream");
-const passportSetup = require("./middlewares/passport");
+
 
 const io = require("socket.io")(http, {
   cors: {
@@ -23,15 +20,6 @@ connectDB();
 // Middlewares
 app.use(express.json());
 app.use(cors());
-app.use(
-  cookieSession({
-    name: "session",
-    keys: ["cyberwolf"],
-    maxAge: 24 * 60 * 60 * 100,
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
 
 // Routes
 app.use(routes);
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,6 @@ const { Router } = require("express");
 const router = Router();
 const userControllers = require("../controllers/usersControllers")
 const { authentication } = require("../middlewares/authentication");
-const passport = require('passport');
 
 //registro usuario
 router.post("/register", userControllers.register);
@@ -10,10 +9,5 @@ router.post("/register", userControllers.register);
 //login usuario
 router.post("/login", authentication, userControllers.login);
 
-//google auth usuario
-router.get("/google/callback", passport.authenticate("google",{
-    successRedirect: process.env.CLIENT_URL,
-    failureRedirect:"/login/failed"
-}))
 
 module.exports = router;
